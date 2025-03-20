86f73aef0b1c571c377f7b3a3120f42f27f8e43a
Binary-Linux <nrw58886@gmail.com>
2025-03-20 14:36:58 +0200

    more adjustments

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ My dotfiles of Niri using Material You colors + Matugen
 ---  
 ![Keybinds](keybinds-binary.png)  
 
+## DESC RU
+эти конфиги я делал когда захотелось эксперементов с Material You когда обычные все возможные GTK темы уже надоели
+pywal тоже надоел, конфиги я делал сам без какой либо помощи и могут быть какие либо баги но зачем это скачевать зная что могут быть баги?
+только если вы также как и я хотите поиграть с Material You и вообще если не уверены лучше
+на VM запускайте.
+
+## DESC EN
+These configs were created when I wanted to experiment with Material You, as I got tired of the every possible usual GTK themes.
+Pywal also became boring, and I made these configs entirely on my own without any assistance, so there might be some bugs. But why download it knowing there could be bugs?
+Only if you, like me, want to play around with Material You. And in general, if you're not sure, it's better to run it on a VM.
+
+## TO-DO
+- [goals]:
+ -install script [ ]
+ -make GTK THEME DEPENDS on wallpaper [X]. requires[mitsugen](https://github.com/DimitrisMilonopoulos/mitsugen)
+ -EWW battery widget [ ]
+
+## OTHERS COOL PROJECTS
+[BSPWM BY SOPHRON](https://github.com/sophragoz/dots)
+[MY COLLECTION OF DOTFILES](https://github.com/binarylinuxx/binary-dots)
--- install.sh
+++ install.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
-echo"hello and welcome to Niri-Material-You Dotfiles"
-echo"WARNING: this script currently pre-pre-alpha! version and probably still left many things to change,add and fix"
+echo "Hello and welcome to Niri-Material-You Dotfiles"
+echo "WARNING: this script is currently in a pre-pre-alpha version and probably still has many things to change, add, and fix"
 
-#BACKUP
-TO_BACKUP="~/.local ~/.config ~/.bashrc"
-BACKUP_DIR="~/.backup"
+# BACKUP
+TO_BACKUP="$HOME/.local $HOME/.config $HOME/.bashrc"
+BACKUP_DIR="$HOME/.backup"
 
-#DO_BACKUP
-echo"> Starting backup...."
-mkdir -p $BACKUP_DIR
-cp -r $TO_BACKUP $BACKUP_DIR
+# DO_BACKUP
+echo "> Starting backup...."
+mkdir -p "$BACKUP_DIR"
+for item in $TO_BACKUP; do
+    cp -r "$item" "$BACKUP_DIR"
+done
 sleep 2
-echo"done [###>      ] 25%"
+echo "Done [###>      ] 25%"
 
-#INSTALL_PACKAGES
-echo"> Starting install dependencis...."
-PKGS="curl wget niri swaybg eww rofi pamixer jq fish-shell alacritty swayimg nerd-font-symbols-ttf papirus-icon-theme"
-sudo xbps-install -S $PKGS
-echo"done [####>     ] 45%"
+# INSTALL PACKAGES
+echo "> Installing dependencies..."
+PKGS="curl wget niri swaybg eww rofi pamixer jq fish-shell alacritty swayimg nerd-fonts papirus-icon-theme"
+sudo xbps-install -Sy $PKGS
+echo "Done [####>     ] 45%"
 
-#THEMES/FONTS
-FONTS_DIR="~/.local/share/fonts/"
-mkdir -p $FONTS_DIR
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-unzip JetBrainsMono-2.304.zip
-cp -r fonts/ttf/* $FONTS_DIR
-echo"done [######>   ] 55%"
+# THEMES/FONTS
+FONTS_DIR="$HOME/.local/share/fonts"
+mkdir -p "$FONTS_DIR"
 
-#COPY_CONFIGS
-TO_COPY="~/Niri-material-you/.config"
-cp -r $TO_COPY ~/
+FONT_ZIP="JetBrainsMono-2.304.zip"
+FONT_URL="https://download.jetbrains.com/fonts/$FONT_ZIP"
+
+echo "> Downloading JetBrains Mono font..."
+wget -q "$FONT_URL" -O "$FONT_ZIP"
+
+if [ -f "$FONT_ZIP" ]; then
+    unzip -q "$FONT_ZIP" -d JetBrainsMono
+    cp -r JetBrainsMono/fonts/ttf/* "$FONTS_DIR"
+    rm -rf JetBrainsMono "$FONT_ZIP"
+    echo "Done [######>   ] 55%"
+else
+    echo "Failed to download JetBrains Mono"
+fi
+
+# COPY CONFIGS
+TO_COPY="$HOME/Niri-material-you/.config"
+echo "> Copying configuration files..."
+cp -r "$TO_COPY" "$HOME/"
+echo "Done [#########] 100%"
+
+echo "Installation completed!"
+
